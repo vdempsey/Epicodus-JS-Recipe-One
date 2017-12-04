b0bfc51885665a8efb3c017184ddfc23abb37709
Rane Fields <rane@>
2017-12-04 14:32:25 -0800

    Add template for angular

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <div class="jumbotron">
+      <h1>_Rename</h1>
+    </div>
+  </div>
+  `
+})
+
+export class AppComponent {
+  // Component logic
+}
