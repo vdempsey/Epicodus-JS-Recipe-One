e5c3e12dba8169db04de6a7f73ec1b4ba100cff5
Guest User <Guest@Epicodus-4A.local>
2017-12-05 08:39:50 -0800

    Build simple example of recipe display

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,12 +5,24 @@ import {Component} from '@angular/core';
   template: `
   <div class="container">
     <div class="jumbotron">
-      <h1>_rename</h1>
+      <h1>Recipe Box</h1>
+      <ul>
+        <li *ngFor="let recipe of recipes">{{recipe.name}}<ul><li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li></ul>{{recipe.directions}}</li>
+      </ul>
     </div>
   </div>
   `
 })
 
 export class AppComponent {
-  // Component logic
+  public recipes: Recipe[] = [];
+
+  constructor() {
+    this.recipes.push(new Recipe("Pizza", ["dough", "cheese"], ["put dough in oven", "enjoy meal"]));
+  }
+}
+
+
+export class Recipe {
+  constructor(public name: string, public ingredients: string[], public directions: string[]) {}
 }
